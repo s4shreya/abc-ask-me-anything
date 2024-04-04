7207fe2b4680cbf55646d95d7364dbe71d2f9993
unknown <s4shreyadhingra@gmail.com>
2024-04-04 18:06:17 +0530

    created pdf upload modal

--- frontend/src/components/PDF interaction panel/Header.jsx
+++ frontend/src/components/PDF interaction panel/Header.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 import styles from "./Header.module.css";
 import logo from "../../assets/images/logo.svg";
 import { FiPlusCircle } from "react-icons/fi";
+import UploadPdfModal from "../upload pdf modal/UploadPdfModal";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const pdfUploadHandler = (file) => {
+    console.log(`uploaded file is: ${file}`);
+  };
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <header className={styles.header}>
       <img src={logo} alt="AI Planet logo" height={41} width={105} />
-      <span className={styles.upload}>
+      <button className={styles.upload} onClick={openModal}>
         <FiPlusCircle className={styles["add-button"]} />
         Upload PDF
-      </span>
+      </button>
+      <UploadPdfModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onUpload={pdfUploadHandler}
+      />
     </header>
   );
 };
---
+++ frontend/src/components/upload pdf modal/UploadPdfModal.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+import styles from "./UploadPdfModal.module.css";
+
+const UploadPdfModal = ({ isOpen, onClose, onUpload }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      onUpload(selectedFile);
+      setSelectedFile(null);
+      onClose();
+    } else {
+      alert("Please select a file to upload.");
+    }
+  };
+
+  const handleClickOutside = (event) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className={`${styles.modal} ${isOpen && styles.open}`}
+      onClick={handleClickOutside}
+    >
+      <div className={styles["modal-content"]}>
+        <span className={styles["close-button"]} onClick={onClose}>
+          &times;
+        </span>
+        <h2>Upload PDF</h2>
+        <div className={styles["input-container"]}>
+          <input
+            type="file"
+            accept=".pdf"
+            id={styles["file-input"]}
+            onChange={handleFileChange}
+          />
+        </div>
+        <button onClick={handleUpload}>Upload PDF</button>
+      </div>
+    </div>
+  );
+};
+
+export default UploadPdfModal;
