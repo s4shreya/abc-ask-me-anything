54a24ce603a4045932a103d35a13590f6063a6c2
s4shreya <s4shreyadhingra.gmail.com>
2024-04-06 17:26:42 +0530

    added starter file to check response of query

--- backend/starter.py
+++ backend/starter.py
@@ -28,6 +28,6 @@ try:
 except Exception as e:
     print("Error:", e)
 
-# query_engine = index.as_query_engine()
-# response = query_engine.query("What did the author do growing up?")
-# print(response)
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
